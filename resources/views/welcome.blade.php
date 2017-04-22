b3c56fee64084c5770be4d35c51459beaf954d35
f1703 <franco170391@gmail.com>
2017-04-22 04:31:56 -0300

    analytics

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -102,7 +102,7 @@
     <header class="header" id="top">
       <div class="container">
         <div class="col-md-6 col-sm-12 col-md-offset3 wrapper" >
-            <br>
+            <br><br><br>
             <h1>TUCUMAN WEB</h1>
             <br>
         </div>
@@ -199,5 +199,15 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-97824932-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
   </body>
 </html>
